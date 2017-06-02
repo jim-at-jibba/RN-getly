7ae86c31a53f63e4feed91f47999d1582e4f9f3b
jim-at-jibba <jim@justjibba.net>
2017-06-02 06:59:03 +0100

    Settings screen base template

---
+++ app/screens/settings.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class SettingsScreen extends Component {d
+  render() {
+    return (
+      <View>
+        <Text>
+          Settings
+        </Text>
+      </View>  
+    );
+  }
+}
+
+export default SettingsScreen;
