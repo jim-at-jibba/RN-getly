f8ac4bdd2a104bb2f8e5d7a28b88b932db671d97
jim-at-jibba <jim@justjibba.net>
2017-06-02 07:20:31 +0100

    Added modal

--- app/screens/home.js
+++ app/screens/home.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { List } from 'react-native-elements';
+import { Alert, View } from 'react-native';
+import { List, Button, Text } from 'react-native-elements';
 import { ListView } from 'realm/react-native';
+import axios from 'axios';
+import Modal from 'react-native-modal';
 
 import Container from '../components/container';
+import SettingsButton from '../components/settings-button';
+import AddFab from '../components/add-fab';
+
 import RequestItem from '../components/request-item';
 import RequestService from '../data/services/requestService';
 
 import config from '../config/config';
 
 class HomeScreen extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerRight: <SettingsButton navigate={navigation.navigate} />
+    };
+  };
+
   getState() {
+    console.log('Initialising state');
     const ds = new ListView.DataSource({
       rowHasChanged: (r1, r2) => r1 !== r2
     });
     let requests = RequestService.findAll();
     return {
       requests,
-      dataSource: ds.cloneWithRows(requests)
+      // dataSource: ds.cloneWithRows(requests),
+      isModalVisible: false,
+      response: {
+        data: {},
+        headers: {},
+        status: null
+      }
     };
   }
 
@@ -28,28 +46,68 @@ class HomeScreen extends Component {
 
     this.deleteRequest = this.deleteRequest.bind(this);
     this.editRequest = this.editRequest.bind(this);
+    this.sendRequest = this.sendRequest.bind(this);
+    this.renderRow = this.renderRow.bind(this);
+  }
+
+  sendRequest(method, url, showResponse) {
+    console.log(showResponse);
+    axios({
+      method,
+      url
+    })
+      .then(response => {
+        if (config.DEV) console.log('Response', response);
+        this.setState({ response });
+
+        if (showResponse) {
+          this.setState({ isModalVisible: true });
+        }
+
+      })
+      .catch(e => console.log('Error:', e));
   }
 
   deleteRequest(id) {
-    if(config.DEV) console.log(`Deleting request with id: ${id}`);
-  }
-
-  editRequest(id) {
-    if(config.DEV) console.log(`Editing request with id: ${id}`);
-  }
-
-  renderRow(rowData) {
-    return (
-      <RequestItem
-        key={rowData.id}
-        data={rowData}
-        delete={(uuid) =>  this.deleteRequest(uuid) }
-        edit={(uuid) => this.editRequest(uuid)}
-      />
+  
+    if (config.DEV) console.log(`Deleting request with id: ${id}`);
+    Alert.alert(
+      'Warning!!',
+      'Are you sure you want to delete this request item?',
+      [
+        { text: 'Cancel', onPress: () => console.log('Cancel Pressed!') },
+        { text: 'OK', onPress: () => {
+          RequestService.deleteOne(id);
+          console.log('Refreshing state');
+          this.setState(this.state);
+        }}
+      ]
     );
   }
 
+  editRequest(id) {
+    if (config.DEV) console.log(`Editing request with id: ${id}`);
+  }
+
+  renderRow() {
+    if(this.state.requests.isValid()) {
+      return this.state.requests.map(rowData => {
+        return (
+          <RequestItem
+            key={rowData.id}
+            data={rowData}
+            delete={id => this.deleteRequest(id)}
+            edit={id => this.editRequest(id)}
+            sendRequest={(method, url, showResponse) => this.sendRequest(method, url, showResponse)}
+          />
+        );
+      });
+    }
+    
+  }
+
   render() {
+    // const htmlContent = `<p>${JSON.stringify(this.state.response.data, null, 4)}</p>`;
     if (this.state.requests.length === 0) {
       return (
         <Container>
@@ -68,14 +126,48 @@ class HomeScreen extends Component {
             marginTop: 0
           }}
         >
-          <ListView
-            dataSource={this.state.dataSource}
-            renderRow={this.renderRow.bind(this)}
-          />
+          {this.renderRow()}
+
         </List>
+        <View style={styles.container}>
+          <Modal isVisible={this.state.isModalVisible}>
+            <View style={styles.modalContent}>
+              <Text h4>Request Response</Text>
+              <Text>Status: {this.state.response.status}</Text>
+              <Text>{JSON.stringify(this.state.response.data, null, 2)}</Text>
+              <Button
+                icon={{
+                  name: 'code'
+                }}
+                title="close"
+                onPress={() => this.setState({ isModalVisible: false })}
+              />
+            </View>
+          </Modal>
+        </View>
+        <AddFab />
       </Container>
     );
   }
 }
 
+const styles = {
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)'
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  pre: {
+    fontWeight: '300',
+    color: '#FF3366' // make links coloured pink
+  }
+};
 export default HomeScreen;
