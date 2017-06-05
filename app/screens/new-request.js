529b8c59c49b27ccc2ea643d7290853795d5b3ac
jim-at-jibba <jim@justjibba.net>
2017-06-05 09:17:44 +0100

    Added radio buttons to add request form

--- app/screens/new-request.js
+++ app/screens/new-request.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, Picker } from 'react-native';
-import { FormLabel, FormInput, Button } from 'react-native-elements';
-import ModalDropdown from 'react-native-modal-dropdown';
+import { View, Text } from 'react-native';
+import { FormLabel, FormInput, CheckBox, Button } from 'react-native-elements';
+import RadioForm, {
+  RadioButton,
+  RadioButtonInput,
+  RadioButtonLabel
+} from 'react-native-simple-radio-button';
 
 import Container from '../components/container';
+import colours from '../config/colours';
 
 class NewRequestScreen extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -18,11 +23,46 @@ class NewRequestScreen extends Component {
     super();
 
     this.state = {
-      checked: true
+      methods: [
+        { label: 'GET', value: 'GET' },
+        { label: 'POST', value: 'POST' },
+        { label: 'PUT', value: 'PUT' },
+        { label: 'DELETE', value: 'DELETE' }
+      ],
+      request: {
+        title: '',
+        url: '',
+        method: '',
+        showReponse: true
+      },
+      methodIndex: 0
     };
+
+    this.methodSelected = this.methodSelected.bind(this);
+    this.saveRequest = this.saveRequest.bind(this);
+  }
+
+  methodSelected(value, index) {
+    this.setState({
+      methodIndex: index,
+      request: { ...this.state.request, value }
+    });
+  }
+
+  saveRequest() {
+    const {title, url, method } = this.state.request;
+
+    if(title === '' ) {
+      let errors = [];
+      errors.push[{title: 'You must provide a title'}]
+      this.setState = {
+        errors: []
+      }
+    }
   }
 
   render() {
+    console.log('STATE:', this.state);
     return (
       <Container form>
         <FormLabel>Title</FormLabel>
@@ -33,11 +73,81 @@ class NewRequestScreen extends Component {
 
         <View>
           <FormLabel>Method</FormLabel>
-          <ModalDropdown options={['option 1', 'option 2']}/>
+          <RadioForm formHorizontal={true} style={styles.radioButtons}>
+            {this.state.methods.map((obj, i) => {
+              return (
+                <RadioButton labelHorizontal={true} key={i}>
+                  <RadioButtonInput
+                    obj={obj}
+                    index={i}
+                    isSelected={this.state.methodIndex === i}
+                    onPress={() => this.methodSelected(obj.value, i)}
+                    buttonInnerColor={colours.primary}
+                    buttonOuterColor={
+                      this.state.methodIndex === i
+                        ? colours.primary
+                        : colours.secondaryText
+                    }
+                    buttonSize={15}
+                    buttonStyle={{}}
+                    buttonWrapStyle={{ marginLeft: 10 }}
+                  />
+                  <RadioButtonLabel
+                    obj={obj}
+                    index={i}
+                    labelHorizontal={true}
+                    onPress={() => this.methodSelected(obj.value, i)}
+                    labelStyle={{ fontSize: 12, color: colours.secondaryText }}
+                    labelWrapStyle={{}}
+                  />
+                </RadioButton>
+              );
+            })}
+          </RadioForm>
         </View>
+
+        <View>
+          <CheckBox
+            title="Show request response?"
+            checked={this.state.request.showResponse}
+            containerStyle={styles.checkBoxContainer}
+            checkedColor={colours.primary}
+            onPress={() => {
+              let showResponse = !this.state.request.showResponse;
+              console.log('SHOW', showResponse);
+              this.setState({
+                request: { ...this.state.request, showResponse }
+              });
+              console.log('Clicked Stae', this.state.request);
+            }}
+          />
+        </View>
+        <Button
+          large
+          iconRight
+          backgroundColor={colours.primary}
+          buttonStyle={styles.buttonStyle}
+          icon={{ name: 'insert-link',  }}
+          title="Save"
+          onPress={() => this.saveRequest()}
+        />
       </Container>
     );
   }
 }
+
+const styles = {
+  radioButtons: {
+    marginTop: 15,
+    marginLeft: 8
+  },
+  checkBoxContainer: {
+    backgroundColor: 'transparent'
+  },
+  buttonStyle: {
+    marginTop: 100,
+    backgroundColor: colours.primary
+  }
+};
 
 export default NewRequestScreen;
